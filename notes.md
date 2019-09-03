29f877b1f7391328b14a7436d3e6f95ebf1ef02a
Transmitator <piotrniewiejski@gmail.com>
2019-09-03 17:27:05 +0200

    "To jest taki nasz mały sukces, co nie? 🦆🦆🦆"

---
+++ notes.md
@@ -0,0 +1,6 @@
+Data sets: 
+- 1 [x] - eg. line 3707
+- 2 [x] - eg. line 28
+- 3 [x] - eg. line 264
+
+The special thing about set no. 3 is that the last column (pistol status) is entirely filled with zeros.
--- src/data_exploring/new-approach.py
+++ src/data_exploring/new-approach.py
@@ -27,17 +27,21 @@ nozzles = NozzlesData()
 def do_stuff_for_nozzles(nozzles, t0, t1):
 
     # select subset of all nozzles between t0 and t1
-    mask = (nozzles["timestamp"] >= t0) & (nozzles["timestamp"] < t1)
+    mask = (nozzles["timestamp"] >= t0) & (nozzles["timestamp"] <= t1)
     nozzles_subset = nozzles.loc[mask]
 
     # iterate over nozzles associated with a given tank
     # TODO: refactor it so it is not hardcoded :p
     for nozzleID in [13, 17, 21]:
-        do_stuff_for_nozzle(nozzles_subset[nozzles_subset["nozzleID"] == nozzleID])
+        print(f"Nozzle: {nozzleID}")
+        do_stuff_for_nozzle(
+            nozzles_subset[nozzles_subset["nozzleID"] == nozzleID], t0, t1
+        )
+        print("**********************************")
 
 
 # TODO: change this name
-def do_stuff_for_nozzle(single_nozzle_subset):
+def do_stuff_for_nozzle(single_nozzle_subset, t0, t1):
     def has_finished_transactions(nozzle):
         """
         Check if nozzle dataframe contains finished transactions.
@@ -46,23 +50,56 @@ def do_stuff_for_nozzle(single_nozzle_subset):
         return nozzle.drop_duplicates("totalCounter", keep="first").shape[0] > 1
 
     def has_ongoing_transactions(nozzle):
+        """nozzle contains non-zero values in literCounter in the analysed time window"""
         return not nozzle[nozzle["literCounter"] > 0].empty
+
+    def countains_transaction_start(nozzle):
+        # problems possible here, pls fix when convenient.
+        liter_counter = nozzle["literCounter"].tolist()
+        [i for i, x in enumerate(liter_counter) if x == 0] > [
+            i for i, x in enumerate(liter_counter) if x > 0
+        ]
+
+    def get_liter_counter_at_timestamp(nozzle, timestamp):
+        nozzle_at_timestamp = nozzle[nozzle["timestamp"] == timestamp]
+        # print(nozzle, timestamp)
+        # print(nozzle_at_timestamp)
+        assert nozzle_at_timestamp.empty == False, "No iternet connection!" 
+        # print(nozzle_at_timestamp["literCounter"].values[0], type(nozzle_at_timestamp["literCounter"]))
+        return nozzle_at_timestamp["literCounter"].values[0]
+
+    nozzle_throughput_sum = 0
 
     if has_ongoing_transactions(single_nozzle_subset) or has_finished_transactions(
         single_nozzle_subset
     ):
+        print("Should return non-zero value :) ")
         # things will happen here!
 
         # check if it contains beginning of transactions
-        liter_counters = single_nozzle_subset["literCounter"].tolist()
-        countains_transaction_start = [
-            i for i, x in enumerate(liter_counters) if x == 0
-        ] > [i for i, x in enumerate(liter_counters) if x > 0]
-        print([l for l in liter_counters])
-        print(
-            countains_transaction_start, has_finished_transactions(single_nozzle_subset)
-        )
-        liter_counters.index(0)
+        if countains_transaction_start(single_nozzle_subset):
+            if get_liter_counter_at_timestamp(single_nozzle_subset, t1) == 0:
+                liter_counter = single_nozzle_subset["totalCounter"].tolist()
+                nozzle_throughput_sum += max(liter_counter) - min(liter_counter)
+            else:
+                nozzle_throughput_sum += get_liter_counter_at_timestamp(
+                    single_nozzle_subset, t1
+                )
+
+        else:
+            if get_liter_counter_at_timestamp(single_nozzle_subset, t1) != 0:
+                nozzle_throughput_sum += get_liter_counter_at_timestamp(
+                    single_nozzle_subset, t1
+                ) - get_liter_counter_at_timestamp(single_nozzle_subset, t0)
+            else:
+                liter_counter = single_nozzle_subset["totalCounter"].tolist()
+                nozzle_throughput_sum += (
+                    max(liter_counter)
+                    - min(liter_counter)
+                    - get_liter_counter_at_timestamp(single_nozzle_subset, t0)
+                )
+
+    print(f"{nozzle_throughput_sum}")
 
         # calculate deltas
         # cases for: beginning of transactions
